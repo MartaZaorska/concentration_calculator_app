8721d8f6b5538f3e85970cb6d3581f2e5ad00381
Marta Zaorska <marta.zaorska2@gmail.com>
2020-04-20 12:33:01 +0200

    Updates

---
+++ precache-manifest.6bca2c0018150fe43887c745f67ba734.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57139c8989c48c64ac2bbaf45a5a84b2",
+    "url": "/concentration_calculator_app/index.html"
+  },
+  {
+    "revision": "f1f0cd3f46a3328a430a",
+    "url": "/concentration_calculator_app/static/css/main.a35a0a33.chunk.css"
+  },
+  {
+    "revision": "0e39364817f4d654dc51",
+    "url": "/concentration_calculator_app/static/js/2.61782fbd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/concentration_calculator_app/static/js/2.61782fbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1f0cd3f46a3328a430a",
+    "url": "/concentration_calculator_app/static/js/main.423799bc.chunk.js"
+  },
+  {
+    "revision": "42908d4400f6d791a063",
+    "url": "/concentration_calculator_app/static/js/runtime-main.5ba60dcf.js"
+  }
+]);
