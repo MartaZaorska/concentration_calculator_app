3b1e30f835836b1ad1951a32e626d334645231c1
Marta Zaorska <marta.zaorska2@gmail.com>
2020-04-23 13:22:50 +0200

    Updates

---
+++ precache-manifest.a1e026982213894c3355232761305e59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86dc4bde4a4352c0ce333d2529779322",
+    "url": "/concentration_calculator_app/index.html"
+  },
+  {
+    "revision": "ee5535f826e674b40e17",
+    "url": "/concentration_calculator_app/static/css/main.aee1808c.chunk.css"
+  },
+  {
+    "revision": "0eb194c01bd6d18ed3aa",
+    "url": "/concentration_calculator_app/static/js/2.032aa1ef.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/concentration_calculator_app/static/js/2.032aa1ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee5535f826e674b40e17",
+    "url": "/concentration_calculator_app/static/js/main.f5b91195.chunk.js"
+  },
+  {
+    "revision": "42908d4400f6d791a063",
+    "url": "/concentration_calculator_app/static/js/runtime-main.5ba60dcf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/concentration_calculator_app/precache-manifest.22924cde91e894f3ce0fb8f4dd0afba0.js"
+  "/concentration_calculator_app/precache-manifest.a1e026982213894c3355232761305e59.js"
 );
 
 self.addEventListener('message', (event) => {
